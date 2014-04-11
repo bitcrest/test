903154ddfd6ff5b9f889a195389f93b0c2c1505e
bitcrest <michael.kh.li@gmail.com>
2014-04-11 19:30:04 +0800

    Added more classes

---
+++ Test GithubTests/Core/EnemyTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Test_Github.Core.Tests
+{
+    [TestFixture]
+    public class EnemyTests
+    {
+        [TearDown]
+        public void TearDown()
+        {
+            _me = null;
+        }
+
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            _me = new Enemy();
+        }
+
+        private Enemy _me;
+
+        [Test]
+        [ExpectedException(typeof(Exception))]
+        public void ReactSpecializationVsPlayerTest()
+        {
+            GameObject go = new Player(1);
+            string x = _me.React(go);
+        }
+    }
+}
